5729e1fdac46f209f98f0ecb6aff5f9885708ed0
Mahmoud <mahmoud.rahat@gmail.com>
2018-01-30 12:43:06 +0330

    1

--- ParsaOIE/ParsaOIE/OieParsa/MatchExtractTuple.cs
+++
@@ -1,9 +0,0 @@
-namespace RahatCoreNlp.OieParsa
-{
-    public class MatchExtractTuple
-    {
-        public PatternNode MatchPattern = new PatternNode();
-        public ExtractionTriple ExtractionPattern = new ExtractionTriple();
-        public string patternStringRawFromFile;  // this field is just for debugging
-    }
-}
